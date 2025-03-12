80264e79e1d8a92cd49f88de9e4b182bb5bd27be
Raff_12 <raffelecoppola381@gmail.com>
2025-03-12 16:09:39 +0100

    feat: Risoluzione del secondo esercizio

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,9 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+const HandleFunctionNameObj = people.map((list)=>{
+    return list.name;
+})
+
+console.log(HandleFunctionNameObj);
