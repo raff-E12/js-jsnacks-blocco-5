bd0a2cf5f13d9f5b9dff1b60086cc71ba77540c5
Raff_12 <raffelecoppola381@gmail.com>
2025-03-12 17:40:08 +0100

    feat: Pubblicazione del quinto esercizio

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,15 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const list_num_pari = nums.map((element)=>{
+    if (element % 2 === 0) {
+        return element = element;
+    } else if (element !== undefined){
+        return element = "";
+    }
+})
+
+let reduce_list = list_num_pari.filter((element) => element !== "");
+
+console.log(reduce_list);
