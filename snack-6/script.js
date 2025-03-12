a7bcc5001c91f775c46470362283db1303f653b9
Raff_12 <raffelecoppola381@gmail.com>
2025-03-12 17:56:20 +0100

    feat: Risoluzione del sesto esercizio

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,24 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+//Lista delle zucchine più grandi di 15cm
+const length_zuc_list = zucchine.map((list, element)=>{
+  if (list.length > 15) {
+    return `${list.type}:${list.length}`;
+  } else if(element !== undefined){
+    return "";
+  }
+}).filter((element)=> element !== "");
+
+//Lista delle zucchine minore di 15cm
+const filter_another = zucchine.map((list, element)=>{
+  if (list.length < 15) {
+    return `${list.type}:${list.length}`;
+  } else if(element !== undefined){
+    return "";
+  }
+}).filter((element)=> element !== "");
+
+console.log(length_zuc_list);
+console.log(filter_another);
