8c70c7850f31fc1583cc2de051f9e2431a4483dc
Raff_12 <raffelecoppola381@gmail.com>
2025-03-12 15:46:57 +0100

    feat: Risoluzione del primo esercizio

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,13 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+let names_element = ()=>{
+    let names_list = [];
+    names.forEach((element, array)=>{
+        names_list.push(element);
+    })
+    return names_list.join("-")
+}
+
+console.log(names_element());
